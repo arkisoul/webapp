88dd6924f5cd910e47396c063b87f37c9d60dddd
Arpit <arpit_jain@live.com>
2019-09-14 14:57:24 +0530

    Initial commit

---
+++ src/containers/Users.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Users extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Users</h1>
+                <p>The awesome users doing this course!</p>
+            </div>
+        )
+    }
+}
+
+export default Users;
